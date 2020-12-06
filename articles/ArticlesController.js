7b35e048c8e167b3ff2425cf6d660c37f492bcdf
Pedro Vinicius <pedrooliveira0490@gmail.com>
2020-12-06 20:33:01 -0300

    update de artigos funcionando e adicionando paginacao as paginas

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -6,16 +6,16 @@ const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
     Article.findAll({
-        include: [{model: Category}]
+        include: [{ model: Category }]
     }).then(articles => {
-        res.render("admin/articles/index", {articles: articles})
+        res.render("admin/articles/index", { articles: articles })
     });
-}); 
+});
 
-router.get("/admin/articles/new", (req ,res) => {
+router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
-        res.render("admin/articles/new",{categories: categories})
-    })    
+        res.render("admin/articles/new", { categories: categories })
+    })
 });
 
 router.post("/articles/save", (req, res) => {
@@ -35,8 +35,8 @@ router.post("/articles/save", (req, res) => {
 
 router.post("/articles/delete", (req, res) => {
     var id = req.body.id;
-    if(id != undefined){
-        if(!isNaN(id)){
+    if (id != undefined) {
+        if (!isNaN(id)) {
             Article.destroy({
                 where: {
                     id: id
@@ -44,12 +44,83 @@ router.post("/articles/delete", (req, res) => {
             }).then(() => {
                 res.redirect("/admin/articles");
             });
-        }else{// NÃO FOR UM NÚMERO
+        } else {// NÃO FOR UM NÚMERO
             res.redirect("/admin/articles");
         }
-    }else{ // NULL
+    } else { // NULL
         res.redirect("/admin/articles");
     }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", { categories: categories, article: article });
+            });
+        } else {
+            res.redirect("/");
+        }
+
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({ title: title, body: body, categoryId: category, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    })
+});
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if (isNaN(page) || page == 1) {
+        offset = 0;
+    } else {
+        offset = (parseInt(page) -1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+
+        var next;
+        if (offset + 4 >= articles.count) {
+            next = false;
+        } else {
+            next = true;
+        }
+
+        var result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", {result: result, categories: categories})
+        });
+
+    })
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const Article = require("./articles/Article");
 const Category = require("./categories/Category");
 
 // View engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 // Static
 app.use(express.static('public'));
 
 //Body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Database
@@ -30,18 +30,19 @@ connection
     })
 
 
-app.use("/",categoriesController);    
-app.use("/",articlesController);
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res) => {
     Article.findAll({
         order: [
             ['id', 'DESC']
-        ]
+        ],
+        limit: 4
     }).then(articles => {
 
         Category.findAll().then(categories => {
-            res.render("index", {articles: articles, categories: categories});
+            res.render("index", { articles: articles, categories: categories });
         });
     });
 });
@@ -53,9 +54,29 @@ app.get("/:slug", (req, res) => {
             slug: slug
         }
     }).then(article => {
-        if(article != undefined) {
+        if (article != undefined) {
             Category.findAll().then(categories => {
-                res.render("article", {article: article, categories: categories});
+                res.render("article", { article: article, categories: categories });
+            });
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { articles: category.articles, categories: categories });
             });
         } else {
             res.redirect("/");
