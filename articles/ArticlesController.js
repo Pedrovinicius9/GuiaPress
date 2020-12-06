eae5ce8815e43ac1438db792bfb69fb74546d5b9
Pedro Vinicius <pedrooliveira0490@gmail.com>
2020-12-06 15:16:52 -0300

    crud de artigos funcionando(menos o update)

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,8 +5,12 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-    res.send("Roda de artigos");
-});
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/index", {articles: articles})
+    });
+}); 
 
 router.get("/admin/articles/new", (req ,res) => {
     Category.findAll().then(categories => {
@@ -27,6 +31,25 @@ router.post("/articles/save", (req, res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     });
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles");
+            });
+        }else{// NÃO FOR UM NÚMERO
+            res.redirect("/admin/articles");
+        }
+    }else{ // NULL
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
